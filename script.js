a657d1211f87b950b793074ca0e65f931e3230a4
Zahra <hizahrazadeh@gmail.com>
2022-01-23 14:34:52 -0500

    add js script for menu toggle

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const menuButton = document.querySelector("menu_button");
-const mobileMenu = document.querySelector("mobile_menu");
+const menuButton = document.getElementsByClassName("menu_button")[0];
+const mobileMenu = document.getElementsByClassName("mobile_menu")[0];
+
+menuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("displayBlock");
+
+  setTimeout(() => {
+    mobileMenu.classList.toggle("opacityOne");
+  }, 100);
+});
