0b0bb8dddd21be77811d51d0a5c0e594ba83e4cf
Zahra <hizahrazadeh@gmail.com>
2022-01-23 14:51:07 -0500

    add toggle for hamburger menu and close

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const menuButton = document.getElementsByClassName("menu_button")[0];
 const mobileMenu = document.getElementsByClassName("mobile_menu")[0];
+const closeButton = document.getElementsByClassName("close_button")[0];
 
 menuButton.addEventListener("click", () => {
   mobileMenu.classList.toggle("displayBlock");
+  closeButton.classList.toggle("displayBlock");
+  menuButton.classList.toggle("displayBlock");
 
   setTimeout(() => {
     mobileMenu.classList.toggle("opacityOne");
+    closeButton.classList.toggle("opacityOne");
   }, 100);
 });
